7808fbb317cac41eb1cfa470f789da724f34c211
Richard Ashkettle <richard.ashkettle@gmail.com>
2025-04-11 13:00:14 -0400

    added player model

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,11 @@
 import { useFBX, useGLTF } from "@react-three/drei";
 import Ground from "./ground";
+import Player from "./Player";
 
 export const Game = () => {
   return (
     <>
+      <Player />
       <Ground />
       <ambientLight intensity={1} />
     </>
